547338c3a835c5b5c178c1d3530e5fb22009dfd4
David <ddk128@gmail.com>
2013-04-01 00:03:30 +0200

    Add hibernate configuration.

--- src/schema/account.groovy
+++ src/schema/account.groovy
@@ -6,5 +6,7 @@ import javax.persistence.*
 @Entity
 @CompileStatic
 public class Account {
+  @Id @GeneratedValue(strategy=GenerationType.AUTO)
+  Long id
   String name
 }
--- src/service/runtime.groovy
+++ src/service/runtime.groovy
@@ -1,7 +1,33 @@
 package service
 
+import org.hibernate.cfg.*
+import schema.Account
+
 class Runtime {
 	static main(args) {
-		println("Hello World!")
+    def properties = [
+      "hibernate.dialect":"org.hibernate.dialect.DerbyDialect",
+      "hibernate.connection.driver_class":"org.apache.derby.jdbc.EmbeddedDriver",
+      "hibernate.connection.url":"jdbc:derby:.data/content;create=true",
+      "hibernate.hbm2ddl.auto":"create-drop",
+      "hibernate.show_sql":"true"]
+
+    def config = new Configuration()
+    properties.each { k, v -> config.setProperty(k, v) }
+    config.addAnnotatedClass(Account)
+
+    def factory = config.buildSessionFactory()
+
+    def session = factory.openSession()
+    session.beginTransaction()
+    session.save(new Account(name:"My Account"))
+    session.transaction.commit()
+    session.close()
+
+    session = factory.openSession()
+    def account = session.createQuery("from Account").uniqueResult()
+
+		println("account: "+account.name)
+    session.close()
 	}
 }

da6771eda47c104a467297af8dd6184f8c4d600e
David <ddk128@gmail.com>
2013-04-05 18:56:54 +0200

    Update for unit tests.

--- src/main/service/runtime.groovy
+++ src/main/service/runtime.groovy
@@ -1,20 +1,17 @@
 package service
 
 import org.springframework.context.support.*
-import org.hibernate.*
-
-import kernel.source.*
 import schema.Account
 
 class Runtime {
   static main(args) {
-    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-    Schema schema = context.getBean("vertex.schema");
+    def context = new ClassPathXmlApplicationContext("context.xml");
+    def schema = context.getBean("vertex.schema");
 
-    Session session = schema.openSession();
+    def session = schema.openSession();
     session.beginTransaction();
 
-    Account account = new Account(name:"My Account");
+    def account = new Account(name:"My Account");
     session.save(account);
     session.getTransaction().commit();
     session.close();

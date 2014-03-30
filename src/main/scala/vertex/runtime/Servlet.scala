be4b1c04610502622dc022493a63fa1699a964fc
David <ddk128@gmail.com>
2014-03-30 04:25:36 +0200

    Add servlet.

---
+++ src/main/scala/vertex/runtime/Servlet.scala
@@ -0,0 +1,13 @@
+package vertex.Servlet
+
+import java.io._
+import javax.servlet._
+import javax.servlet.http._
+
+class Servlet extends HttpServlet {
+  override def doGet(request:HttpServletRequest, response:HttpServletResponse) {
+    val writer = response.getWriter()
+    writer.println("Fuck Off! I'm busy.")
+    writer.close()
+  }
+}
